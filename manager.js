33b5d8bc2750cc245f64b3018005e51e8198959c
Rafat <bellorafat1@gmail.com>
2023-05-17 20:17:35 -0400

    4 solved

--- manager.js
+++ manager.js
@@ -9,6 +9,20 @@ class Manager extends Employee {
     addEmployee(employee) {
         this.employees.push(employee);
     }
+
+    calculateBonus(multiplier) {
+        return (this.salary + this._totalSubSalary()) * multiplier
+    }
+
+    _totalSubSalary() {
+        let sum = 0;
+        for (let i = 0; i < this.employees.length; i++) {
+            const employee = this.employees[i];
+            if (employee instanceof Manager) sum += employee.salary + employee._totalSubSalary();
+            else sum += employee.salary
+        }
+        return sum
+    }
 }
 
 module.exports = Manager;
